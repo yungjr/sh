9aef892a1e0f6aa4c1267689cb2a562e2ce431ef
“yungjr” <pcusb999@gmail.com>
2023-08-20 01:26:30 +0800

    修改

--- kejilion.sh
+++ kejilion.sh
@@ -2382,7 +2382,7 @@ case $choice in
           1)
               clear
               read -p "请输入你的快捷按键: " kuaijiejian
-              echo "alias $kuaijiejian='curl -sS -O https://raw.githubusercontent.com/yungjr/sh/main/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh'" >> ~/.bashrc
+              echo "alias $kuaijiejian='curl -sS -O https://raw.githubusercontent.com/yungjr/sh/dev/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh'" >> ~/.bashrc
               echo "快捷键已添加。请重新启动终端，或运行 'source ~/.bashrc' 以使修改生效。"
               ;;
 
